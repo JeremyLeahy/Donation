1f51a4f8572d78c54a0f6c4635e13725a47bd309
jeremyleahy <Tregender@10>
2020-11-12 21:41:38 +0000

    Set up of progrees bar and amount picker, adding donate button

--- app/src/main/java/ie/wit/Donate.kt
+++ app/src/main/java/ie/wit/Donate.kt
@@ -5,21 +5,56 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_donate.*
+import kotlinx.android.synthetic.main.activity_donate.fab
+import kotlinx.android.synthetic.main.content_donate.*
 
 class Donate : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donate)
         setSupportActionBar(toolbar)
 
+        //setting up progress Bar and amount picker
+        progressBar.max = 10000
+        amountPicker.minValue = 1
+        amountPicker.maxValue = 1000
+
+        //setting a change listener on that amount picker-that's how to enable the payment amount edit text
+        amountPicker.setOnValueChangedListener { _, _, newVal ->
+            //Display the newly selected number to paymentAmount
+            paymentAmount.setText("$newVal")
+        }
+
+        /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+         */
+
+        var totalDonated = 0
+
+        donateButton.setOnClickListener {
+            val amount = if (paymentAmount.text.isNotEmpty())
+                paymentAmount.text.toString().toInt() else amountPicker.value
+            if(totalDonated >= progressBar.max)
+                Toast.makeText(this,"Donate Amount Exceeded!",Toast.LENGTH_LONG).show()
+            else {
+                totalDonated += amount
+                totalSoFar.text = "$$totalDonated"
+                progressBar.progress = totalDonated
+            }
+        }
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
